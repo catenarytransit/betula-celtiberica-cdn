6a7cedcb17e21534e7d45e80046e261035e958dd
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-02-19 15:19:20 -0800

    add to workflow

---
+++ fetch_tlms.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+fetch("https://birch.catenarymaps.org/getroutesofchateau/deutschland")
+    .then((response) => response.json())
+    .then((data) => {
+        let results = data.filter((r) => r.agency_id === "401").map((x) => {return {route_id: x.route_id, short_name: x.short_name}}).sort((a, b) => a.route_id - b.route_id);
+
+        fs.writeFile("dresden_routes.json", JSON.stringify(results, null, 2), (err) => {
+            if (err) {
+                console.error("Error writing file:", err);
+            } else {
+                console.log("dresden_routes.json successfully written!");
+            }
+        });
+    })
+    .catch((error) => {
+        console.error("Fetch error:", error);
+    });
